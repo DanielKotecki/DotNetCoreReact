ff1b33bee2e418420adb8a7391048c62d14c281f
Daniel Kotecki <daniel.kotecki98@gmail.com>
2022-07-10 15:04:11 +0200

    Database and Seed

---
+++ Persistence/Migrations/20220710124844_InitialCreate2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Data",
+                table: "Activities",
+                newName: "Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Activities",
+                newName: "Data");
+        }
+    }
+}
